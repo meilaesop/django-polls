9436b2d53b7f7346bed400b314930db40bfca626
meilaesop <meilaesop@gmail.com>
2025-12-11 19:09:08 +0800

    更新urls.py添加认证URL路由，更新settings.py认证设置

--- django_polls/urls.py
+++ django_polls/urls.py
@@ -1,15 +1,22 @@
 from django.urls import path
 from . import views
 
-app_name="polls"
-urlpatterns=[
-    #ex:/polls/
-    path("",views.IndexView.as_view(),name="index"),
-    #ex:/polls/5/
-    path("<int:pk>/",views.DetailView.as_view(),name="detail"),
-    #ex:/polls/5/results/
-    path("<int:pk>/results/",views.ResultsView.as_view(),name="results"),
-    #ex:/polls/5/vote/
-    path("<int:question_id>/vote/",views.vote,name="vote"),
-]
+app_name = 'polls'
 
+urlpatterns = [
+    # 原有的投票URL - 保持原样
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    
+    # 🔥 新增用户认证URL
+    # 注册
+    path('register/', views.register, name='register'),
+    # 登录
+    path('login/', views.custom_login, name='login'),
+    # 退出
+    path('logout/', views.custom_logout, name='logout'),
+    # 个人资料
+    path('profile/', views.profile, name='profile'),
+]
